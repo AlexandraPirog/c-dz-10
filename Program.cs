798fc2276358915bb4cb6d133109f42cf88d5cfe
Alex <pirogav53@gmail.com>
2022-12-06 22:12:07 +0300

    Задача 10 решена

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>99&&num<1000)
+{
+string chr = Convert.ToString(num);
+System.Console.WriteLine ($"{chr[1]}");
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число");
+}
+
+
+/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
